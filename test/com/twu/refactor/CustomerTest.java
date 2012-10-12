00278881ad91f1589385b81d9ee57a95e3018448
Asif Momin <asifmom@thoughtworks.com>
2012-10-12 15:09:50 +0530

    refactoring the test: clarified intent in CustomerTest

--- test/com/twu/refactor/CustomerTest.java
+++ test/com/twu/refactor/CustomerTest.java
@@ -9,47 +9,47 @@ import junit.framework.TestCase;
 
 public class CustomerTest extends TestCase {
 
-	private static final String GOLD_PATH = "test/data";
+	private static final String TEST_DATA_ROOT = "test/data";
 
-    private Customer dinsdale = new Customer("Dinsdale Pirhana");
+    private Customer dinsdaleCustomer = new Customer("Dinsdale Pirhana");
 
-    private Movie python = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
-	private Movie ran = new Movie("Ran", Movie.REGULAR);
-	private Movie la = new Movie("LA Confidential", Movie.NEW_RELEASE);
-	private Movie trek = new Movie("Star Trek 13.2", Movie.NEW_RELEASE);
-	private Movie wallace = new Movie("Wallace and Gromit", Movie.CHILDRENS);
+    private Movie pythonMovie = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
+	private Movie ranMovie = new Movie("Ran", Movie.REGULAR);
+	private Movie laMovie = new Movie("LA Confidential", Movie.NEW_RELEASE);
+	private Movie trekMovie = new Movie("Star Trek 13.2", Movie.NEW_RELEASE);
+	private Movie wallaceMovie = new Movie("Wallace and Gromit", Movie.CHILDRENS);
 
     public void setUp (){
-       dinsdale.addRental(new Rental (python, 3));
-       dinsdale.addRental(new Rental (ran, 1));
-       dinsdale.addRental(new Rental (la, 2));
-       dinsdale.addRental(new Rental (trek, 1));
-       dinsdale.addRental(new Rental (wallace, 6));
+       dinsdaleCustomer.addRental(new Rental(pythonMovie, 3));
+       dinsdaleCustomer.addRental(new Rental(ranMovie, 1));
+       dinsdaleCustomer.addRental(new Rental(laMovie, 2));
+       dinsdaleCustomer.addRental(new Rental(trekMovie, 1));
+       dinsdaleCustomer.addRental(new Rental(wallaceMovie, 6));
    }
 
-    public void testEmpty() throws Exception {
-    	dinsdale = new Customer("Dinsdale Pirhana");
-        equalsFile("1st Output", "outputEmpty", dinsdale.statement());
+    public void testStatementWithoutAnyRentals() throws Exception {
+    	Customer customerWithoutAnyRental = new Customer("Dinsdale Pirhana");
+        equalsFile("outputEmpty", customerWithoutAnyRental.statement());
     }
-    public void testCustomer() throws Exception {
-        equalsFile("1st Output", "output1", dinsdale.statement());
+    public void testStatementWithRentals() throws Exception {
+        equalsFile("output1", dinsdaleCustomer.statement());
     }
 
-    public void testChange() throws Exception {
-    	la.setPriceCode(Movie.REGULAR);
-        equalsFile("1st Output", "outputChange", dinsdale.statement());
+    public void testStatementShouldReflectChangeInMoviePriceCode() throws Exception {
+    	laMovie.setPriceCode(Movie.REGULAR);
+        equalsFile("outputChange", dinsdaleCustomer.statement());
     }
 
     /*
     public void testHtml() throws Exception {
-        equalsFile("1st Output", "outputHtml", dinsdale.htmlStatement());
+        equalsFile("1st Output", "outputHtml", dinsdaleCustomer.htmlStatement());
     }
     */
     	
-    protected void equalsFile(String message, String fileName, String actualValue) throws IOException{
-        BufferedReader file = new BufferedReader (new FileReader (GOLD_PATH + '/' + fileName));
+    protected void equalsFile(String fileName, String actualValue) throws IOException{
+        BufferedReader file = new BufferedReader (new FileReader (TEST_DATA_ROOT + '/' + fileName));
         BufferedReader actualStream = new BufferedReader (new StringReader (actualValue));
-        String thisFileLine = null;
+        String thisFileLine;
         while  ((thisFileLine = file.readLine()) != null) {
             assertEquals ("in file: " + fileName, thisFileLine, actualStream.readLine());
         }
